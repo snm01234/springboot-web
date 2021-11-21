0f8767fe8f6a03b31f09878bd7dae5f5c63f6a42
snm01234 <park96618@gmail.com>
2021-11-21 16:54:44 +0900

    jpa, h2 설정 / 등록,수정,조회 api 작성

---
+++ src/main/java/com/snm01234/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.snm01234.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/main/java/com/snm01234/springboot/web/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.snm01234.springboot.web;
+
+import com.snm01234.springboot.service.PostsService;
+import com.snm01234.springboot.web.dto.PostsResponseDto;
+import com.snm01234.springboot.web.dto.PostsSaveRequestDto;
+import com.snm01234.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return  postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
