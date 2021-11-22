f763401000b09a5126bed950a5ff31015d9ea873
snm01234 <park96618@gmail.com>
2021-11-22 16:08:00 +0900

    mustache로 화면 구성. 게시글 등록/조회/수정/삭제 화면과 기능 추가

--- src/main/java/com/snm01234/springboot/service/PostsService.java
+++ src/main/java/com/snm01234/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.snm01234.springboot.service;
 
 import com.snm01234.springboot.domain.posts.Posts;
 import com.snm01234.springboot.domain.posts.PostsRepository;
+import com.snm01234.springboot.web.dto.PostsListResponseDto;
 import com.snm01234.springboot.web.dto.PostsResponseDto;
 import com.snm01234.springboot.web.dto.PostsSaveRequestDto;
 import com.snm01234.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -25,6 +29,19 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위 유지하면서 조회기능만 남겨두어 조회속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == map.(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
