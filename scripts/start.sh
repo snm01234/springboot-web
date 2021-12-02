d29b56fc124647450d0aa7d18bd6973936c935e1
snm01234 <park96618@gmail.com>
2021-12-02 18:01:22 +0900

    무중단배포 적용

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=springboot-web
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
