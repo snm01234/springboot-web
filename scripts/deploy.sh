18e6fff090e5ab79900e235c2d48705eaffa4f04
snm01234 <park96618@gmail.com>
2021-12-02 16:51:22 +0900

    deploy 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=springboot-web
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -23,7 +23,7 @@ fi
 
 echo "새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar|tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar|tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -8,12 +8,12 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot-web
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
