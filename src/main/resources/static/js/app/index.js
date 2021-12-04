684c11ba0c16470c5b30f7fbdbd63b8a4ee70b07
snm01234 <park96618@gmail.com>
2021-12-04 19:19:57 +0900

    rollback commit test

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -82,23 +82,6 @@ var main = {
             window.location.href = '/';
         }
 
-    },
-    uploadImage : function () {
-        var file = $('#img')[0].files[0];
-        var formData = new FormData();
-        formData.append('data', file);
-
-        $.ajax({
-            type: 'POST',
-            url: '/upload',
-            data: formData,
-            processData: false,
-            contentType: false
-        }).done(function (data)) {
-            $('#result-image').attr("src", data);
-        }).fail(function (error) {
-            alert(error);
-        })
     }
 
 };
