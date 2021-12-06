acbf1c045152f8f0855d40dd4a839d8de3ffb346
snm01234 <park96618@gmail.com>
2021-12-06 14:59:31 +0900

    deploy용 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,7 +34,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-aws.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-credentials.properties \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
